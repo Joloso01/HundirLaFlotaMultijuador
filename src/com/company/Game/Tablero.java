2e918abb7c0e26ea52ab16173b4078abcb0869be
jose <jbermejop01@elpuig.xeill.net>
2021-05-14 17:51:34 +0200

    metodo rellenar tablero añadido

--- src/com/company/Game/Tablero.java
+++ src/com/company/Game/Tablero.java
@@ -7,11 +7,68 @@ public class Tablero {
     private String[][] tablero_PosicionBarcos;
     private String[][] tablero_jugadores;
     public Map<String,Integer> jugadores_conectados;
+    private boolean colocado = false;
 
 
     public Tablero() {
         tablero_PosicionBarcos = new String[10][10];
         tablero_jugadores = new String[10][10];
+        rellenarTableroPosicion();
+    }
+
+    private void rellenarTableroPosicion() {
+        int x, y;
+
+        for (int i = 0; i < 3; i++) {
+            while (!colocado){
+                x = (int) (Math.random()*9);
+                y = (int) (Math.random()*9);
+                if (tablero_PosicionBarcos[x][y] == null){
+                    tablero_PosicionBarcos[x][y] = "F";
+                    colocado = true;
+                }
+
+            }
+            colocado = false;
+        }
+
+        for (int i = 0; i < 4; i++) {
+            while (!colocado){
+                x = (int) (Math.random()*9);
+                y = (int) (Math.random()*9);
+                if (tablero_PosicionBarcos[x][y] == null){
+                    tablero_PosicionBarcos[x][y] = "D";
+                    colocado = true;
+                }
+
+            }
+            colocado = false;
+        }
+
+        for (int i = 0; i < 1; i++) {
+            while (!colocado){
+                x = (int) (Math.random()*9);
+                y = (int) (Math.random()*9);
+                if (tablero_PosicionBarcos[x][y] == null){
+                    tablero_PosicionBarcos[x][y] = "P";
+                    colocado = true;
+                }
+
+            }
+            colocado = false;
+        }
+
+
+
+        for (int i = 0; i < 10; i++) {
+            for (int j = 0; j < 10; j++) {
+                if (tablero_PosicionBarcos[i][j] == null){
+                    tablero_PosicionBarcos[i][j] = "A";
+                }
+                System.out.print(tablero_PosicionBarcos[i][j]+" || ");
+            }
+            System.out.println();
+        }
     }
 
     public void addPlayer(String nombre){
